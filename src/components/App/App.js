d8c689edbb575ff823166779ba8ae1580b89d389
joshfry <joshdfry@me.com>
2020-12-14 16:52:20 -0800

    Setup

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="module">
+        <h1>App</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
